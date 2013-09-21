f2230d69b4e9c8e630d5f4415ba8122446933336
Daniel Prynych <Daniel@prynych.cz>
2013-09-21 22:58:03 +0200

    21.9.2013 22:57

---
+++ Vydejna/PrijemkaMaterialu.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Vydejna
+{
+    partial class PrijemkaMaterialu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelDosudKs = new System.Windows.Forms.Label();
+            this.labelVyrobce = new System.Windows.Forms.Label();
+            this.labelJK = new System.Windows.Forms.Label();
+            this.labeNazev = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.numericUpDownPrijemKs = new System.Windows.Forms.NumericUpDown();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBoxPoznamka = new System.Windows.Forms.TextBox();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePickerDatum = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrijemKs)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Název nářadi :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Označení JK :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 71);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Výrobce :";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labelDosudKs);
+            this.groupBox1.Controls.Add(this.labelVyrobce);
+            this.groupBox1.Controls.Add(this.labelJK);
+            this.groupBox1.Controls.Add(this.labeNazev);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(473, 135);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Popis nařadí";
+            // 
+            // labelDosudKs
+            // 
+            this.labelDosudKs.AutoSize = true;
+            this.labelDosudKs.Location = new System.Drawing.Point(144, 100);
+            this.labelDosudKs.Name = "labelDosudKs";
+            this.labelDosudKs.Size = new System.Drawing.Size(0, 13);
+            this.labelDosudKs.TabIndex = 16;
+            // 
+            // labelVyrobce
+            // 
+            this.labelVyrobce.AutoSize = true;
+            this.labelVyrobce.Location = new System.Drawing.Point(144, 71);
+            this.labelVyrobce.Name = "labelVyrobce";
+            this.labelVyrobce.Size = new System.Drawing.Size(0, 13);
+            this.labelVyrobce.TabIndex = 15;
+            // 
+            // labelJK
+            // 
+            this.labelJK.Location = new System.Drawing.Point(144, 43);
+            this.labelJK.Name = "labelJK";
+            this.labelJK.Size = new System.Drawing.Size(0, 13);
+            this.labelJK.TabIndex = 14;
+            // 
+            // labeNazev
+            // 
+            this.labeNazev.AutoSize = true;
+            this.labeNazev.Location = new System.Drawing.Point(144, 16);
+            this.labeNazev.Name = "labeNazev";
+            this.labeNazev.Size = new System.Drawing.Size(0, 13);
+            this.labeNazev.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Dosud vedeno na kartě :";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(18, 174);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(70, 13);
+            this.label12.TabIndex = 17;
+            this.label12.Text = "Příjem kusů :";
+            // 
+            // numericUpDownPrijemKs
+            // 
+            this.numericUpDownPrijemKs.Enabled = false;
+            this.numericUpDownPrijemKs.Location = new System.Drawing.Point(124, 172);
+            this.numericUpDownPrijemKs.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDownPrijemKs.Name = "numericUpDownPrijemKs";
+            this.numericUpDownPrijemKs.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDownPrijemKs.TabIndex = 18;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(18, 233);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(63, 13);
+            this.label13.TabIndex = 19;
+            this.label13.Text = "Poznámka :";
+            // 
+            // textBoxPoznamka
+            // 
+            this.textBoxPoznamka.Location = new System.Drawing.Point(124, 230);
+            this.textBoxPoznamka.MaxLength = 60;
+            this.textBoxPoznamka.Name = "textBoxPoznamka";
+            this.textBoxPoznamka.Size = new System.Drawing.Size(359, 20);
+            this.textBoxPoznamka.TabIndex = 20;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(12, 274);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 21;
+            this.buttonCancel.Text = "Zrušit";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(408, 274);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 22;
+            this.buttonOK.Text = "Budiž";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Datum :";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // dateTimePickerDatum
+            // 
+            this.dateTimePickerDatum.Enabled = false;
+            this.dateTimePickerDatum.Location = new System.Drawing.Point(124, 201);
+            this.dateTimePickerDatum.Name = "dateTimePickerDatum";
+            this.dateTimePickerDatum.Size = new System.Drawing.Size(193, 20);
+            this.dateTimePickerDatum.TabIndex = 28;
+            // 
+            // PrijemkaMaterialu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 314);
+            this.Controls.Add(this.dateTimePickerDatum);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.textBoxPoznamka);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.numericUpDownPrijemKs);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "PrijemkaMaterialu";
+            this.Text = "PrijemkaMaterialu";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrijemKs)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.NumericUpDown numericUpDownPrijemKs;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBoxPoznamka;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDatum;
+        private System.Windows.Forms.Label labelDosudKs;
+        private System.Windows.Forms.Label labelVyrobce;
+        private System.Windows.Forms.Label labelJK;
+        private System.Windows.Forms.Label labeNazev;
+    }
+}
---
+++ Vydejna/PrijemkaMaterialu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Vydejna
+{
+    public partial class PrijemkaMaterialu : Form
+    {
+
+
+
+        public PrijemkaMaterialu()
+        {
+            InitializeComponent();
+        }
+
+
+        public PrijemkaMaterialu(Hashtable DBRow, vDatabase myDataBase)
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
