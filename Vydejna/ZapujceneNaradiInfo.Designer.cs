51c595f6f3cb290c74f75521282d13d12611846d
Daniel Prynych <Daniel@prynych.cz>
2014-01-02 23:18:30 +0100

    2.1.2013 23:18

---
+++ Vydejna/ZapujceneNaradiInfo.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace Vydejna
+{
+    partial class ZapujceneNaradiInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelNazev = new System.Windows.Forms.Label();
+            this.labelJK = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelRozmer = new System.Windows.Forms.Label();
+            this.labelCSN = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.labelPoznamka = new System.Windows.Forms.Label();
+            this.labelVevcislo = new System.Windows.Forms.Label();
+            this.labelCena = new System.Windows.Forms.Label();
+            this.labelDatum = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.labelPNazev = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Název :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "JK :";
+            // 
+            // labelNazev
+            // 
+            this.labelNazev.AutoSize = true;
+            this.labelNazev.Location = new System.Drawing.Point(125, 13);
+            this.labelNazev.Name = "labelNazev";
+            this.labelNazev.Size = new System.Drawing.Size(35, 13);
+            this.labelNazev.TabIndex = 2;
+            this.labelNazev.Text = "label3";
+            // 
+            // labelJK
+            // 
+            this.labelJK.AutoSize = true;
+            this.labelJK.Location = new System.Drawing.Point(125, 36);
+            this.labelJK.Name = "labelJK";
+            this.labelJK.Size = new System.Drawing.Size(35, 13);
+            this.labelJK.TabIndex = 3;
+            this.labelJK.Text = "label3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Rozměr :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "ČSN :";
+            // 
+            // labelRozmer
+            // 
+            this.labelRozmer.AutoSize = true;
+            this.labelRozmer.Location = new System.Drawing.Point(125, 62);
+            this.labelRozmer.Name = "labelRozmer";
+            this.labelRozmer.Size = new System.Drawing.Size(35, 13);
+            this.labelRozmer.TabIndex = 6;
+            this.labelRozmer.Text = "label5";
+            // 
+            // labelCSN
+            // 
+            this.labelCSN.AutoSize = true;
+            this.labelCSN.Location = new System.Drawing.Point(125, 88);
+            this.labelCSN.Name = "labelCSN";
+            this.labelCSN.Size = new System.Drawing.Size(35, 13);
+            this.labelCSN.TabIndex = 7;
+            this.labelCSN.Text = "label5";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Poznamka :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 142);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Interní ev. číslo :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(60, 167);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Cena :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(3, 194);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Datum zapůjčení :";
+            // 
+            // labelPoznamka
+            // 
+            this.labelPoznamka.AutoSize = true;
+            this.labelPoznamka.Location = new System.Drawing.Point(125, 114);
+            this.labelPoznamka.Name = "labelPoznamka";
+            this.labelPoznamka.Size = new System.Drawing.Size(35, 13);
+            this.labelPoznamka.TabIndex = 12;
+            this.labelPoznamka.Text = "label9";
+            // 
+            // labelVevcislo
+            // 
+            this.labelVevcislo.AutoSize = true;
+            this.labelVevcislo.Location = new System.Drawing.Point(125, 142);
+            this.labelVevcislo.Name = "labelVevcislo";
+            this.labelVevcislo.Size = new System.Drawing.Size(35, 13);
+            this.labelVevcislo.TabIndex = 13;
+            this.labelVevcislo.Text = "label9";
+            // 
+            // labelCena
+            // 
+            this.labelCena.AutoSize = true;
+            this.labelCena.Location = new System.Drawing.Point(125, 167);
+            this.labelCena.Name = "labelCena";
+            this.labelCena.Size = new System.Drawing.Size(35, 13);
+            this.labelCena.TabIndex = 14;
+            this.labelCena.Text = "label9";
+            // 
+            // labelDatum
+            // 
+            this.labelDatum.AutoSize = true;
+            this.labelDatum.Location = new System.Drawing.Point(125, 194);
+            this.labelDatum.Name = "labelDatum";
+            this.labelDatum.Size = new System.Drawing.Size(35, 13);
+            this.labelDatum.TabIndex = 15;
+            this.labelDatum.Text = "label9";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(533, 250);
+            this.tabControl1.TabIndex = 16;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.labelDatum);
+            this.tabPage1.Controls.Add(this.labelNazev);
+            this.tabPage1.Controls.Add(this.labelCena);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.labelVevcislo);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.labelPoznamka);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.labelCSN);
+            this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Controls.Add(this.labelRozmer);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.labelJK);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(525, 224);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Současné údaje";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage2.Controls.Add(this.labelPNazev);
+            this.tabPage2.Controls.Add(this.label9);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(525, 224);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "V době výpujčky";
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(12, 279);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 31;
+            this.buttonCancel.Text = "Zrušit";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(42, 14);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(44, 13);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Název :";
+            // 
+            // labelPNazev
+            // 
+            this.labelPNazev.AutoSize = true;
+            this.labelPNazev.Location = new System.Drawing.Point(102, 14);
+            this.labelPNazev.Name = "labelPNazev";
+            this.labelPNazev.Size = new System.Drawing.Size(41, 13);
+            this.labelPNazev.TabIndex = 2;
+            this.labelPNazev.Text = "label10";
+            // 
+            // ZapujceneNaradiInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 307);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "ZapujceneNaradiInfo";
+            this.Text = "ZapujceneNaradiInfo";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelNazev;
+        private System.Windows.Forms.Label labelJK;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelRozmer;
+        private System.Windows.Forms.Label labelCSN;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label labelPoznamka;
+        private System.Windows.Forms.Label labelVevcislo;
+        private System.Windows.Forms.Label labelCena;
+        private System.Windows.Forms.Label labelDatum;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Label labelPNazev;
+        private System.Windows.Forms.Label label9;
+    }
+}
---
+++ Vydejna/ZapujceneNaradiInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Vydejna
+{
+    public partial class ZapujceneNaradiInfo : Form
+    {
+        private Hashtable DBRow;
+
+        public ZapujceneNaradiInfo(Hashtable DBRow)
+        {
+            InitializeComponent();
+            this.DBRow = DBRow;
+            setData();
+        }
+
+
+        private void setData()
+        {
+            if (DBRow.Contains("nazev")) labelNazev.Text = Convert.ToString(DBRow["nazev"]);
+            else labelNazev.Text = "";
+            if (DBRow.Contains("rozmer")) labelRozmer.Text = Convert.ToString(DBRow["rozmer"]);
+            else labelRozmer.Text = "";
+            if (DBRow.Contains("jk")) labelJK.Text = Convert.ToString(DBRow["jk"]);
+            else labelJK.Text = "";
+            if (DBRow.Contains("normacsn")) labelCSN.Text = Convert.ToString(DBRow["normacsn"]);
+            else labelCSN.Text = "";
+            //----------------------
+            if (DBRow.Contains("pnazev")) labelPNazev.Text = Convert.ToString(DBRow["pnazev"]);
+            else labelPNazev.Text = "";
+
+
+        }
+    }
+}
